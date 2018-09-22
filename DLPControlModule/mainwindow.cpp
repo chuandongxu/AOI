1936a9f20e43c4af9fb1b27d2ee2ece71e2e8e7a
ningfeiguang <ningfeiguang@163.com>
2018-09-22 19:16:44 +0800

    DLP save path bug
    1. Fixed bug of the DLP Saving path.

--- DLPControlModule/mainwindow.cpp
+++ DLPControlModule/mainwindow.cpp
@@ -2399,9 +2399,12 @@ void MainWindow::on_pushButton_SaveSolution_clicked()
 
     //if(fileName.isEmpty())
     //    return;
+    
+    fileName = fileName.toLower();
+    QDir dir(path);
+    QString dirPath = dir.absolutePath().toLower();	
 
-	QDir dir(path);
-	if (fileName.isEmpty() || fileName.indexOf(dir.absolutePath()) < 0)
+    if (fileName.isEmpty() || fileName.indexOf(dirPath) < 0)
 	{
 		return;
 	}
@@ -2415,7 +2418,8 @@ void MainWindow::on_pushButton_SaveSolution_clicked()
 	{
 		path = QApplication::applicationDirPath();
 		QDir dir(path);
-		QString szSavePath = str.remove(dir.absolutePath());
+        QString dirPath = dir.absolutePath().toLower();
+        QString szSavePath = str.remove(dirPath);
 
         bool bMotionCardTrigger = System->isHardwareTrigger();
 		System->setParam((bMotionCardTrigger ? QString("dlp_default_solution_file_path_motion") : QString("dlp_default_solution_file_path")) + QString("_%1").arg(m_nDLPIndex + 1), szSavePath);
