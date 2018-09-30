f875c117ffbb7599ca2ca432f395d9dfd832ef44
ningfeiguang <ningfeiguang@163.com>
2018-09-30 12:13:47 +0800

    3D View Center Optmized
    1. Modify the 3D vision view to enable user rotate the view at the test device center.

--- 3DViewUtility/RoamingScenceManager.h
+++ 3DViewUtility/RoamingScenceManager.h
@@ -67,8 +67,8 @@ private:
     Vector3D *AuxZ;
 
     //旋转后观察点方向与视线向上方向
-    Vector3D*NewEye;
-    Vector3D*NewUp;
+    Vector3D *NewEye;
+    Vector3D *NewUp;
     Vector3D *NewView;
 
     Position3D *OldMouse;
